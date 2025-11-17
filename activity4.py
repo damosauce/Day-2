11e3aadae0ddd822f2a7e713ee133c965b126d2f
damosauce <damosauce>
2025-11-17 16:55:34 -0500

    commit

--- activity4.py
+++ activity4.py
@@ -1,7 +1,12 @@
 x= input("Enter a number: ")
 y= input("Enter another number: ")
+b= input("Enter one more number: ")
 temp=x
 x=y
+b=y
+b=x
 y=temp
+b=temp
 print("Value of x After mapping ", x)
 print("Value of y After mapping ", y)
+print("Value of b After mapping ", b)
